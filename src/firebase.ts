f425703149db328bdc711010e8f8afed767cf840
Le Minh Tri <ansidev@gmail.com>
2019-01-03 17:41:25 +0700

    Trim trailing white space and format code

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,8 +4,8 @@ export class FirebaseHelper {
 
     /**
      * Initialize Firebase App
-     * 
-     * @param {any} serviceAccount 
+     *
+     * @param {any} serviceAccount
      * @param {any} databaseURL
      */
     initializeApp(serviceAccount: string, databaseURL: string) {
@@ -18,7 +18,7 @@ export class FirebaseHelper {
 
     /**
      * Get User Info though UserId
-     * 
+     *
      * @param {string} userId
      * @returns {Promise<any>}
      */
@@ -30,7 +30,7 @@ export class FirebaseHelper {
 
     /**
      * Get User Info though email
-     * 
+     *
      * @param {string} email
      * @returns {Promise<any>}
      * @memberof FirebaseHelper
@@ -43,7 +43,7 @@ export class FirebaseHelper {
 
     /**
      * Get User Info though phone number
-     * 
+     *
      * @param {string} phone
      * @returns {Promise<any>}
      * @memberof FirebaseHelper
@@ -54,17 +54,17 @@ export class FirebaseHelper {
             .catch(error => console.log(error))
     }
 
-    
+
     /**
      * Delete a user
-     * 
+     *
      * @param {Array<string>} userIds
      * @memberof FirebaseHelper
      */
     deleteUser(userId: string): Promise<boolean> {
         return new Promise(resolve => {
             admin.auth().deleteUser(userId)
-                .then(() => {                    
+                .then(() => {
                     resolve(true);
                 })
                 .catch(error => {
@@ -76,7 +76,7 @@ export class FirebaseHelper {
 
     /**
      * Delete user from an Array of User Ids
-     * 
+     *
      * @param {Array<string>} userIds
      * @memberof FirebaseHelper
      */
@@ -101,24 +101,24 @@ export class FirebaseHelper {
     createUser(userInfo: Object): Promise<object> {
         return new Promise((resolve) => {
             admin.auth().createUser(userInfo)
-            .then( (userRecord) => {                
-                resolve({
-                    status: true,
-                    data: userRecord
+                .then((userRecord) => {
+                    resolve({
+                        status: true,
+                        data: userRecord
+                    })
                 })
-            })
-            .catch( (error) => {                
-                resolve({
-                    status: false,
-                    data: error.message
-                })
-            });
-        })        
+                .catch((error) => {
+                    resolve({
+                        status: false,
+                        data: error.message
+                    })
+                });
+        })
     }
 
     /**
      * Update user Info though userId
-     * 
+     *
      * @param {string} userId
      * @param {Object} userInfo
      * @memberof FirebaseHelper
@@ -126,13 +126,13 @@ export class FirebaseHelper {
     updateUser(userId: string, userInfo: Object): Promise<object> {
         return new Promise(resolve => {
             admin.auth().updateUser(userId, userInfo)
-                .then( userRecord => {                    
+                .then(userRecord => {
                     resolve({
                         status: true,
                         data: userRecord
                     })
                 })
-                .catch( (error) => {                    
+                .catch((error) => {
                     resolve({
                         status: false,
                         data: error.message
@@ -143,16 +143,16 @@ export class FirebaseHelper {
 
     /**
      * Get a list of users
-     * 
-     * @param {number} [maxResults=1000] 
-     * @returns {Promise<any>} 
+     *
+     * @param {number} [maxResults=1000]
+     * @returns {Promise<any>}
      * @memberof FirebaseHelper
      */
-    getAllUsers(maxResults: number = 1000): Promise<any>{
+    getAllUsers(maxResults: number = 1000): Promise<any> {
         return admin.auth().listUsers(maxResults)
-        .then((listUsersResult) => listUsersResult.users)
-        .catch((error) => {
-            console.log("Error listing users:", error);
-        });
+            .then((listUsersResult) => listUsersResult.users)
+            .catch((error) => {
+                console.log("Error listing users:", error);
+            });
     }
 }
--- src/firestore.ts
+++ src/firestore.ts
@@ -12,14 +12,14 @@ export class FirestoreHelper {
      * @returns {Promise<boolean>}
      * @memberof FirestoreHelper
      */
-    createDocumentWithID (db: any, collectionName: string, docId: string, data: Object): Promise<boolean> {
+    createDocumentWithID(db: any, collectionName: string, docId: string, data: Object): Promise<boolean> {
         return db.collection(collectionName).doc(docId).set(data)
             .then(() => true)
-            .catch(function (error) {                
+            .catch(function (error) {
                 return false;
             });
     }
-    
+
     /**
      * Create a document without an ID
      *
@@ -29,27 +29,27 @@ export class FirestoreHelper {
      * @returns
      * @memberof FirestoreHelper
      */
-    createNewDocument (db: any, collectionName: string, data: Object): Promise<any> {
+    createNewDocument(db: any, collectionName: string, data: Object): Promise<any> {
         return db.collection(collectionName).add(data)
-            .then(function (docRef) {                
+            .then(function (docRef) {
                 return docRef;
             })
-            .catch(function (error) {                
+            .catch(function (error) {
                 return error;
             });
     }
 
-     /**
-     * Create a document with id in firestore
-     * 
-     * @param {any} db 
-     * @param {any} collectionName 
-     * @param {any} docID
-     * @param {any} collectionNameTwo
-     * @param {any} docIDTwo
-     * @param {any} data 
-     * @returns 
-     */
+    /**
+    * Create a document with id in firestore
+    *
+    * @param {any} db
+    * @param {any} collectionName
+    * @param {any} docID
+    * @param {any} collectionNameTwo
+    * @param {any} docIDTwo
+    * @param {any} data
+    * @returns
+    */
     // createSubcollectionWithDocument (db: any, collectionName: string, docId: string, collectionNameTwo: string, docIdTwo: string, data: Object): void {
     //     db.collection(collectionName).doc(docId).collection(collectionNameTwo).doc(docIdTwo)).set(data)
     //         .then(res => console.log(`${JSON.stringify(data)} is added to ${collectionNameTwo} collection`))
@@ -66,7 +66,7 @@ export class FirestoreHelper {
      * @returns {Promise<any>}
      * @memberof FirestoreHelper
      */
-    updateDocument (db: any, collectionName: string, docId: string, data: Object): Promise<any> {
+    updateDocument(db: any, collectionName: string, docId: string, data: Object): Promise<any> {
         return db.collection(collectionName).doc(docId).update(data)
             .then(() => true)
             .catch(err => err)
@@ -81,25 +81,25 @@ export class FirestoreHelper {
      * @returns {Promise<object>}
      * @memberof FirestoreHelper
      */
-    deleteDocument (db: any, collectionName: string, docId: string): Promise<object> {
+    deleteDocument(db: any, collectionName: string, docId: string): Promise<object> {
         return db.collection(collectionName).doc(docId).delete()
-            .then(() => {                
-                return {status: true, message: `${docId} successfully deleted!`};
-            }).catch(error => {                
-                return {status: true, message: error};
+            .then(() => {
+                return { status: true, message: `${docId} successfully deleted!` };
+            }).catch(error => {
+                return { status: true, message: error };
             });
     }
-  
-     /**
-     * Delete a document from Sub Collection
-     * 
-     * @param {any} db 
-     * @param {any} collectionName 
-     * @param {any} docId 
-     * @param {any} collectionNameTwo
-     * @param {any} docIdTwo
-     */
-    deleteDocumentFromSubcollection (db: any, collectionName: string, docId: string, collectionNameTwo: string, docIdTwo: string) {
+
+    /**
+    * Delete a document from Sub Collection
+    *
+    * @param {any} db
+    * @param {any} collectionName
+    * @param {any} docId
+    * @param {any} collectionNameTwo
+    * @param {any} docIdTwo
+    */
+    deleteDocumentFromSubcollection(db: any, collectionName: string, docId: string, collectionNameTwo: string, docIdTwo: string) {
         db.collection(collectionName).doc(docId).collection(collectionNameTwo).doc(docIdTwo).delete()
             .then(() => {
                 console.log(`${docIdTwo} successfully deleted!`);
@@ -110,15 +110,15 @@ export class FirestoreHelper {
 
     /**
     * Check where a document exists or not
-    * 
-    * @param {any} db 
-    * @param {string} collectionName 
-    * @param {string} docId 
-    * @returns {Promise<any>} 
+    *
+    * @param {any} db
+    * @param {string} collectionName
+    * @param {string} docId
+    * @returns {Promise<any>}
     * @memberof FirestoreHelper
     */
 
-    checkDocumentExists(db: any, collectionName: string, docId: string): Promise<any>{
+    checkDocumentExists(db: any, collectionName: string, docId: string): Promise<any> {
         const dbRef = db.collection(collectionName).doc(docId);
         return dbRef.get()
             .then(doc => {
@@ -140,16 +140,16 @@ export class FirestoreHelper {
 
     /**
      * Get a document from document Id
-     * 
-     * @param {any} db 
-     * @param {string} collectionName 
-     * @param {string} documentId 
-     * @returns {Promise<any>} 
+     *
+     * @param {any} db
+     * @param {string} collectionName
+     * @param {string} documentId
+     * @returns {Promise<any>}
      * @memberof FirestoreHelper
      */
     getDocument(db: any, collectionName: string, documentId: string): Promise<any> {
         const docRef = db.collection(collectionName).doc(documentId);
-        return docRef.get().then(function(doc) {
+        return docRef.get().then(function (doc) {
             if (doc.exists) {
                 return doc.data();
             } else {
@@ -157,21 +157,21 @@ export class FirestoreHelper {
                 console.log("No such document!");
                 return false;
             }
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log("Error getting document:", error);
         });
     }
 
     /**
      * Query data from Firestore
-     * 
-     * @param {*} db 
-     * @param {string} collectionName 
-     * @param {[any]} queryArray 
-     * @returns {Promise<any>} 
+     *
+     * @param {*} db
+     * @param {string} collectionName
+     * @param {[any]} queryArray
+     * @returns {Promise<any>}
      * @memberof FirestoreHelper
      */
-    queryData(db: any, collectionName: string, queryArray: [any]): Promise<any>{
+    queryData(db: any, collectionName: string, queryArray: [any]): Promise<any> {
         return new Promise((resolve, reject) => {
             const dataRef = db.collection(collectionName);
             const queryRef = dataRef.where(queryArray[0], queryArray[1], queryArray[2]);
@@ -182,7 +182,7 @@ export class FirestoreHelper {
                     snapshot.forEach(doc => {
                         results[doc.id] = doc.data();
                     });
-                    if(Object.keys(results).length > 0){
+                    if (Object.keys(results).length > 0) {
                         resolve(results);
                     } else {
                         resolve('No such document!');
@@ -203,7 +203,7 @@ export class FirestoreHelper {
      * @param {string} subCollection
      * @return {json}
      */
-    backup (db: any, collectionName: string, subCollection: string = '') {
+    backup(db: any, collectionName: string, subCollection: string = '') {
         // console.log('Getting data from: ', collectionName);
         return new Promise((resolve, reject) => {
             const data = {};
@@ -243,12 +243,12 @@ export class FirestoreHelper {
 
     /**
      * Get sub collection from a document if possible
-     * 
-     * @param {any} db 
-     * @param {any} data 
-     * @param {any} dt 
-     * @param {any} collectionName 
-     * @param {any} subCollection 
+     *
+     * @param {any} db
+     * @param {any} data
+     * @param {any} dt
+     * @param {any} collectionName
+     * @param {any} subCollection
      */
     async getSubCollection(db: any, data: Object, dt: Object, collectionName: string, subCollection: string) {
         for (const [key, value] of Object.entries([dt[collectionName]][0])) {
@@ -259,13 +259,13 @@ export class FirestoreHelper {
 
     /**
      * Add sub collection to data object if possible
-     * 
-     * @param {any} db 
-     * @param {any} key 
-     * @param {any} subData 
-     * @param {any} collectionName 
-     * @param {any} subCollection 
-     * @returns 
+     *
+     * @param {any} db
+     * @param {any} key
+     * @param {any} subData
+     * @param {any} collectionName
+     * @param {any} subCollection
+     * @returns
      */
     addSubCollection(db: any, key: string, subData: Object, collectionName: string, subCollection: string) {
         return new Promise((resolve, reject) => {
@@ -290,7 +290,7 @@ export class FirestoreHelper {
      * @returns {Promise<any>}
      * @memberof FirestoreHelper
      */
-    restore (db, fileName): Promise<any> {
+    restore(db, fileName): Promise<any> {
         const that = this;
         return new Promise((resolve, reject) => {
             fs.readFile(fileName, 'utf8', function (err, data) {
@@ -298,24 +298,24 @@ export class FirestoreHelper {
                     console.log(err);
                     return;
                 }
-    
+
                 // Turn string from file to an Array
                 const dataArray = JSON.parse(data);
-    
-                that.updateCollection(db, dataArray).then(() => {                    
-                    resolve({status: true, message: 'Successfully import collection!'});
-                }).catch(error => {                    
-                    reject({status: false, message: error.message});
+
+                that.updateCollection(db, dataArray).then(() => {
+                    resolve({ status: true, message: 'Successfully import collection!' });
+                }).catch(error => {
+                    reject({ status: false, message: error.message });
                 });
-            })  
+            })
         })
     }
 
     /**
      * Update data to Firestore
-     * 
+     *
      * @param {any} db
-     * @param {any} dataArray 
+     * @param {any} dataArray
      */
     async updateCollection(db: any, dataArray: Object) {
         for (const index in dataArray) {
@@ -329,12 +329,12 @@ export class FirestoreHelper {
 
     /**
      * Write data to document
-     * 
-     * @param {any} db 
-     * @param {any} collectionName 
-     * @param {any} doc 
-     * @param {any} data 
-     * @returns 
+     *
+     * @param {any} db
+     * @param {any} collectionName
+     * @param {any} doc
+     * @param {any} data
+     * @returns
      */
     startUpdating(db: any, collectionName: string, doc: string, data: Object) {
         return new Promise(resolve => {
